2bc90a364134de03e45c79fb38a012a38a4c9be7
tugcedmr <67045607+tugcedmr@users.noreply.github.com>
2020-07-09 19:17:54 +0300

    Update ExcelYazdir.py

--- ExcelYazdir.py
+++ ExcelYazdir.py
@@ -3,20 +3,20 @@ import re
 from openpyxl import Workbook,load_workbook
 wb = load_workbook("deneme\MorEvimAktifScript.xlsx")
 ws = wb.active
-os.system("md morevim1")
-os.chdir(r'C:\Users\LENOVO\Desktop\morevim1')
+os.system("md morevim1")  #klasör olusturma
+os.chdir(r'C:\Users\LENOVO\Desktop\morevim1')   #klasöre gitme
 for dongu in range(2,ws.max_row+1):
     os.chdir(r'C:\Users\LENOVO\Desktop\morevim1')
     Gsutun=ws["G"+format(dongu)].value
     Isutun=ws["I"+format(dongu)].value
     klasorSorgusu=Gsutun.replace(" ", "")
-    def DosyaYazdir():
-        dosya = open(str(Gsutun.replace(" ", ""))+(str(re.sub(r"\s+$", "",Isutun, flags=re.UNICODE))+format(dongu-1))+'.js','w+',encoding="utf-8")
+    def DosyaYazdir():   
+        dosya = open(str(Gsutun.replace(" ", ""))+(str(re.sub(r"\s+$", "",Isutun, flags=re.UNICODE))+format(dongu-1))+'.js','w+',encoding="utf-8")  #dosyayı açma ve isim verme
         satir="\n //"+str(ws["G"+format(dongu)].value)
         satir=satir+"\n //"+str(ws["I"+format(dongu)].value)
         satir=satir+"\n //"+str(ws["J"+format(dongu)].value)
         satir=satir+"\n"+str(ws["K"+format(dongu)].value)
-        dosya.write(satir)
+        dosya.write(satir) 
         dosya.close()
     if os.path.exists(klasorSorgusu)==True:
         os.chdir(r'C:\Users\LENOVO\Desktop\morevim1')
